cbd2a7b2362b9a4644971e6c7754261480e78d4d
Ailton Kenji Akiba <aakiba@Kenji-PC>
2022-10-24 12:32:34 -0300

    :bug: fix swap push

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: akenji-a <akenji-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 21:21:41 by akenji-a          #+#    #+#             */
-/*   Updated: 2022/10/20 12:12:40 by akenji-a         ###   ########.fr       */
+/*   Updated: 2022/10/24 12:30:13 by akenji-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,15 @@ int		main(int argc, char *argv[])
 	if (argc > 1)
 	{
 		if (args_parse(++argv, stacks))
-			ft_printf("Bom garoto\n");
+		{
+			ft_printf("Fez\n");
+			print_stacks(stacks);
+			ft_printf("Depois do push\n");
+			operation("sa", stacks);
+			print_stacks(stacks);
+		}
 		else
-			ft_printf("Mau garoto\n");
+			ft_printf("Nao fez\n");
 	}
-	print_stacks(stacks);
-	ft_printf("Depois do push\n");
-	operation("pb", stacks);
-	operation("pb", stacks);
-	operation("pb", stacks);
-	operation("pa", stacks);
-	print_stacks(stacks);
 	return (0);
 }
